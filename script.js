32ce3ab15b671fd218cc7d9b1cead5995eb4c622
ShotgunR <onlywesley@hotmail.com>
2023-05-22 23:42:42 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = function () {
-
-
-}
-
-
+  const chart = new d3Kit.SvgChart("#chart0", {
+    initialWidth: 720,
+    initialHeight: 500,
+    margin: {left: 20, right: 20, top: 20, bottom: 30},
+    offset: [0.5, 0.5], // add little offset for sharp-edge rendering
+  });
+  chart.rootG.append('circle')
+  .attr('cx', 10)
+  .attr('cy', 10)
+  .attr('r', 5)
+};
