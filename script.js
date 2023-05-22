011582736f0c3399a27ab95825b084580386aef5
wh-zhero <wh@zhero.co.uk>
2023-05-22 14:57:22 +0100

    update

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const t = urlParams.get('t');
 console.log(t);
-const div = d3.select("body")
-.selectAll("div")
-.data([4, 8, 15, 16, 23, 42])
-.enter().append("div").text(d => d);
+
+
+let p = d3.select('body')
+  .selectAll('div')
+  .data([3, 4]);
+
+const pEnter = p.enter()
+  .append('div')
+  .text(d => d);
+  
+p.text(d => d);
